788874f7951684d6178e5758bfe3aeffcc085892
rolf <a@b.c>
2017-11-13 21:18:51 +0100

    Core: Fix ADC register declaration

--- core/Device/NXP/LPC54xxx/Include/LPC5411x.h
+++ core/Device/NXP/LPC54xxx/Include/LPC5411x.h
@@ -8,14 +8,14 @@
 
 typedef enum IRQn {
     /******  Cortex-M4 Processor Exceptions Numbers ***************************************************/
-    NonMaskableInt_IRQn         = -14,      /*!< 2 Non Maskable Interrupt                         */
-    MemoryManagement_IRQn       = -12,      /*!< 4 Cortex-M3 Memory Management Interrupt          */
-    BusFault_IRQn               = -11,      /*!< 5 Cortex-M3 Bus Fault Interrupt                  */
-    UsageFault_IRQn             = -10,      /*!< 6 Cortex-M3 Usage Fault Interrupt                */
-    SVCall_IRQn                 = -5,       /*!< 11 Cortex-M3 SV Call Interrupt                   */
-    DebugMonitor_IRQn           = -4,       /*!< 12 Cortex-M3 Debug Monitor Interrupt             */
-    PendSV_IRQn                 = -2,       /*!< 14 Cortex-M3 Pend SV Interrupt                   */
-    SysTick_IRQn                = -1,       /*!< 15 Cortex-M3 System Tick Interrupt               */
+    NonMaskableInt_IRQn         = -14,
+    MemoryManagement_IRQn       = -12,
+    BusFault_IRQn               = -11,
+    UsageFault_IRQn             = -10,
+    SVCall_IRQn                 = -5,
+    DebugMonitor_IRQn           = -4,
+    PendSV_IRQn                 = -2,
+    SysTick_IRQn                = -1,
 
     /******  LPC5411x Specific Interrupt Numbers *******************************************************/
     WDT_BOD_IRQn                = 0,
@@ -140,7 +140,6 @@ typedef struct {
             __I  uint32_t DAT9;
             __I  uint32_t DAT10;
             __I  uint32_t DAT11;
-            __I  uint32_t DAT12;
         };
     };
     __IO uint32_t THR0_LOW;
