b13e5d64b20eea91145386b8d7df4eb036e0533e
Rolf Meeser <rolf@black.local>
2018-11-21 00:13:23 +0100

    Version 33.0

--- src/app.h
+++ src/app.h
@@ -5,8 +5,8 @@
 #include "scanner.h"
 #include "sys.h"
 
-#define FIRMWARE_VERSION_MAJOR              32
-#define FIRMWARE_VERSION_MINOR              1
+#define FIRMWARE_VERSION_MAJOR              33
+#define FIRMWARE_VERSION_MINOR              0
 #define FIRMWARE_NAME                       ""
 
 #if (BOARD_RA == 1)
