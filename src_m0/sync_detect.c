a62b016efaf6f1e59e933bdcc42d2b733af2e635
Rolf Meeser <rolf@black.local>
2019-04-12 22:20:53 +0200

    Add Biphase-Space capability to M0 bit processing; support MEISEI sondes

--- src_m0/sync_detect.c
+++ src_m0/sync_detect.c
@@ -12,17 +12,20 @@ struct _SyncDetectorContext {
     uint64_t rxShiftReg[2];
     const SYNC_Config *config;
 
-    int state;                  /* 0=sync hunt, 1=data reception */
+    SYNC_State state;
     int activeBuffer;
     int detectedType;           /* 0=RS92, 1=RS41, 2=DFM06, 3=DFM09 */
-    int frameLength;
-    int rxCounter;
+    int frameLengthBits;
+    int rxCounterBits;
     int bitCounter;
     int writeIndex;
     int uartBitCounter;
-    SYNC_BitFormat bitFormat;
     bool inverted;
+    int lastBit;
     PostProcessFunc postProcess;
+    int symbolPhase;
+    int nSubBlockBits;
+    int nSubBlockBytes;
 } syncContext;
 
 
@@ -45,7 +48,7 @@ void PIN_INT3_IRQHandler (void)
         bit = GPIO_readBit(GPIO_RX_DATA);
 
         switch (handle->state) {
-            case 0:           /* Sync hunt */
+            case SYNC_STATE_HUNT:
                 /* Take RX bit and put into 128-bit shift register */
                 handle->rxShiftReg[1] <<= 1;
                 if (handle->rxShiftReg[0] & (1ull << 63)) {
@@ -82,15 +85,17 @@ void PIN_INT3_IRQHandler (void)
                         }
                         if (j < IPC_S2M_NUM_BUFFERS) {
                             handle->activeBuffer = j;
-                            handle->frameLength = handle->config->conf[i].frameLength;
-                            handle->rxCounter = handle->config->conf[i].frameLength;
+                            handle->frameLengthBits = handle->config->conf[i].frameLengthBits;
+                            handle->rxCounterBits = handle->config->conf[i].frameLengthBits;
                             handle->bitCounter = 0;
-                            handle->bitFormat = handle->config->conf[i].bitFormat;
+                            handle->state = handle->config->conf[i].dataState;
                             handle->inverted = handle->config->conf[i].inverted;
                             handle->uartBitCounter = 0;
                             handle->writeIndex = handle->config->conf[i].startOffset;
                             handle->postProcess = handle->config->conf[i].postProcess;
-                            handle->state = 1;
+                            handle->symbolPhase = 0;
+                            handle->nSubBlockBits = handle->config->conf[i].nSubBlockBits;
+                            handle->nSubBlockBytes = handle->config->conf[i].nSubBlockBytes;
                             ipc_s2m[handle->activeBuffer].opcode = 0;
                             ipc_s2m[handle->activeBuffer].param = handle->config->conf[i].id;
                         }
@@ -98,23 +103,50 @@ void PIN_INT3_IRQHandler (void)
                 }
                 break;
 
-            case 1:           /* Data reception */
+            case SYNC_STATE_DATA_RAW:
+                if (handle->inverted) {
+                    bit = bit ^ 1;
+                }
+
+                ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] =
+                    (ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] << 1) | bit;
+
+                if (++handle->bitCounter >= 8) {
+                    handle->bitCounter = 0;
+                    ++handle->writeIndex;
+                }
+
+                if (--handle->rxCounterBits <= 0) {
+                    handle->state = SYNC_STATE_HUNT;
+                    handle->writeIndex = 0;
+                    if (handle->postProcess) {
+                        handle->postProcess(&ipc_s2m[handle->activeBuffer]);
+                    }
+                    ipc_s2m[handle->activeBuffer].valid = 1;
+
+                    LPC_MAILBOX->IRQ1SET = (1u << 0);
+                }
+                else if (handle->rxCounterBits == handle->frameLengthBits / 2) {
+                    /* Tell M4 to keep current RSSI value */
+                    LPC_MAILBOX->IRQ1SET = (1u << 1);
+                }
+                break;
+
+            case SYNC_STATE_DATA_UART_8N1:
                 skipBit = false;
 
-                if (handle->bitFormat == SYNC_BITFORMAT_UART_8N1) {
-                    /* Strip start and stop bit */
-                    if (handle->uartBitCounter == 0) {
-                        skipBit = true;
-                    }
-                    if (handle->uartBitCounter == 9) {
-                        skipBit = true;
-                    }
+                /* Strip start and stop bit */
+                if (handle->uartBitCounter == 0) {
+                    skipBit = true;
+                }
+                if (handle->uartBitCounter == 9) {
+                    skipBit = true;
+                }
 
-                    /* 8N1: 10 bits per character */
-                    ++handle->uartBitCounter;
-                    if (handle->uartBitCounter >= 10) {
-                        handle->uartBitCounter = 0;
-                    }
+                /* 8N1: 10 bits per character */
+                ++handle->uartBitCounter;
+                if (handle->uartBitCounter >= 10) {
+                    handle->uartBitCounter = 0;
                 }
 
                 if (!skipBit) {
@@ -122,34 +154,82 @@ void PIN_INT3_IRQHandler (void)
                         bit = bit ^ 1;
                     }
 
-                    if (handle->bitFormat == SYNC_BITFORMAT_UART_8N1) {
-                        ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] =
-                            (ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] >> 1) | (bit << 7);
-                    }
-                    else {
-                        ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] =
-                            (ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] << 1) | bit;
-                    }
+                    ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] =
+                        (ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] >> 1) | (bit << 7);
 
                     if (++handle->bitCounter >= 8) {
                         handle->bitCounter = 0;
                         ++handle->writeIndex;
-                        if (--handle->rxCounter <= 0) {
-                            handle->state = 0;
-                            handle->writeIndex = 0;
-                            if (handle->postProcess) {
-                                handle->postProcess(&ipc_s2m[handle->activeBuffer]);
-                            }
-                            ipc_s2m[handle->activeBuffer].valid = 1;
+                    }
 
-                            LPC_MAILBOX->IRQ1SET = (1u << 0);
+                    if (--handle->rxCounterBits <= 0) {
+                        handle->state = SYNC_STATE_HUNT;
+                        handle->writeIndex = 0;
+                        if (handle->postProcess) {
+                            handle->postProcess(&ipc_s2m[handle->activeBuffer]);
                         }
-                        else if (handle->rxCounter == handle->frameLength / 2) {
-                            /* Tell M4 to keep current RSSI value */
-                            LPC_MAILBOX->IRQ1SET = (1u << 1);
-                        }
+                        ipc_s2m[handle->activeBuffer].valid = 1;
+
+                        LPC_MAILBOX->IRQ1SET = (1u << 0);
+                    }
+                    else if (handle->rxCounterBits == handle->frameLengthBits / 2) {
+                        /* Tell M4 to keep current RSSI value */
+                        LPC_MAILBOX->IRQ1SET = (1u << 1);
                     }
                 }
+                break;
+
+            case SYNC_STATE_DATA_BIPHASE_S:
+                if (handle->symbolPhase == 0) {
+                    /* First bit in a Manchester symbol: Remember for later evaluation */
+                    handle->lastBit = bit;
+                    ++handle->symbolPhase;
+                }
+                else {
+                    if ((handle->bitCounter % 8) == 0) {
+                        ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] = 0;
+                    }
+
+                    /* Second bit in a Manchester symbol: Determine and save data bit */
+                    int offset = handle->bitCounter % 8;
+                    bit = (bit == handle->lastBit) ? 1 : 0;
+                    ipc_s2m[handle->activeBuffer].data8[handle->writeIndex] |= (bit << offset);
+
+                    handle->symbolPhase = 0;
+
+                    ++handle->bitCounter;
+                    if ((handle->bitCounter % 8) == 0) {
+                        ++handle->writeIndex;
+                    }
+                    if (handle->nSubBlockBits != 0) {
+                        if (handle->bitCounter >= handle->nSubBlockBits) {
+                            handle->bitCounter = 0;
+                            int bits2skip = 8 * handle->nSubBlockBytes - handle->nSubBlockBits;
+                            if (bits2skip > 0) {
+                                handle->writeIndex += 1 + (bits2skip - 1) / 8;
+                            }
+                        }
+                    }
+
+                    if (--handle->rxCounterBits <= 0) {
+                        handle->state = SYNC_STATE_HUNT;
+                        handle->writeIndex = 0;
+                        if (handle->postProcess) {
+                            handle->postProcess(&ipc_s2m[handle->activeBuffer]);
+                        }
+                        ipc_s2m[handle->activeBuffer].valid = 1;
+
+                        LPC_MAILBOX->IRQ1SET = (1u << 0);
+                    }
+                    else if (handle->rxCounterBits == handle->frameLengthBits / 2) {
+                        /* Tell M4 to keep current RSSI value */
+                        LPC_MAILBOX->IRQ1SET = (1u << 1);
+                    }
+                }
+                break;
+
+            default:    /* Should never come here! */
+                handle->state = SYNC_STATE_HUNT;
                 break;
         }
     }
