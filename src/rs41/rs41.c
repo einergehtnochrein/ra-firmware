c784224b5076f0ea02279ffb57d27cb2169bd9b4
Rolf Meeser <rolf@black.local>
2019-10-21 01:51:42 +0200

    RS41: Begin with better XDATA handling

--- src/rs41/rs41.c
+++ src/rs41/rs41.c
@@ -108,11 +108,30 @@ static bool _RS41_checkCRC (uint8_t *buffer, int length, uint16_t receivedCRC)
 
 
 
-static void _RS41_readSubFrameAux (char *p, int length, RS41_InstanceData *instance)
+static void _RS41_readSubFrameXdata (char *p, int length, RS41_InstanceData *instance)
 {
-(void)p;
-    if (length == 21) {
-        instance->metro.hasO3 = true;
+    /* Minimum required data in XDATA subframe:
+     * 1 byte unknown
+     * Instrument ID (type of instrument), 2 ASCII digits
+     * Instrument number (position in chain), 2 ASCII digits
+     */
+    if (length >= 5) {
+        /* For the most part we leave decoding XDATA packets to the app.
+         * However, we want to know if an ozone unit is present,
+         * so we check for specific instrument ID's.
+         */
+        int instrumentID;
+        int instrumentNumber;
+        if (sscanf(p, "%*c%02d%02d", &instrumentID, &instrumentNumber) == 2) {
+            switch (instrumentID) {
+                case 5:
+                    /* Sanity check for packet length */
+                    if (length == 21) {
+                        instance->metro.hasO3 = true;
+                    }
+                    break;
+            }
+        }
     }
 }
 
@@ -493,8 +512,8 @@ LPCLIB_Result RS41_processBlock (RS41_Handle handle, void *buffer, uint32_t leng
                     snprintf(handle->instance->nameVariant, sizeof(handle->instance->nameVariant), "%s", "RS41-SGM");
                     handle->instance->logMode = RS41_LOGMODE_RAW;
                     break;
-                case RS41_SUBFRAME_AUX:
-                    _RS41_readSubFrameAux((char *)(p + 2), subFrameLength - 4, handle->instance);
+                case RS41_SUBFRAME_XDATA:
+                    _RS41_readSubFrameXdata((char *)(p + 2), subFrameLength - 4, handle->instance);
                     break;
                 default:
                     /* Ignore unknown (or unhandled) frame types */
