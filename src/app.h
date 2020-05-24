6b5866646865297d87b7a280a145e92f593aa879
Rolf Meeser <rolf@black.local>
2020-05-24 14:55:53 +0200

    Version 42.1 (only RA1)

--- src/app.h
+++ src/app.h
@@ -7,7 +7,7 @@
 #include "sys.h"
 
 #define FIRMWARE_VERSION_MAJOR              42
-#define FIRMWARE_VERSION_MINOR              0
+#define FIRMWARE_VERSION_MINOR              1
 #define FIRMWARE_NAME                       ""
 
 #if (BOARD_RA == 1)
