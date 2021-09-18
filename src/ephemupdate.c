einergehtnochrein/ra-firmware

commit 89da4a752882b74fc806eadd7a7d5aa485e661dc

Address various compiler/IDE warnings regarding xxx...printf

src/ephemupdate.c
@@ -24,6 +24,7 @@
  */
 
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -108,7 +109,7 @@ LPCLIB_Result EPHEMUPDATE_open (EPHEMUPDATE_Handle *pHandle)
 
 static void _EPHEMUPDATE_sendResponse (int action, const char *status)
 {
-    char s[20];
+    char s[24];
     sprintf(s, "%d,%s", action, status);
     SYS_send2Host(HOST_CHANNEL_EPHEMUPDATE, s);
 }
@@ -415,7 +416,7 @@ LPCLIB_Result EPHEMUPDATE_processCommand (EPHEMUPDATE_Handle handle, const char 
             {
                 uint32_t checksum = _EPHEMUPDATE_getEphemerisChecksum();
                 static char s[20];
-                sprintf(s, "%ld", checksum);
+                sprintf(s, "%"PRIx32, checksum);
                 response = s;
             }
             break;

src/beacon/beacon.c
@@ -1,4 +1,5 @@
 
+#include <inttypes.h>
 #include <math.h>
 #if !defined(M_PI)
 #  define M_PI 3.14159265358979323846
@@ -69,7 +70,7 @@ static void _BEACON_sendKiss (BEACON_InstanceData *instance)
         special |= 2;
     }
 
-    length = snprintf(s, sizeof(s), "%ld,9,%.3f,,%.5lf,%.5lf,,,,,,,%d,,,,%.1f,,,,,",
+    length = snprintf(s, sizeof(s), "%"PRIu32",9,%.3f,,%.5lf,%.5lf,,,,,,,%d,,,,%.1f,,,,,",
                     instance->id,
                     instance->rxFrequencyMHz,           /* Frequency [MHz] */
                     lat,
@@ -82,7 +83,7 @@ static void _BEACON_sendKiss (BEACON_InstanceData *instance)
     }
 
     /* Beacon 15 Hex ID */
-    snprintf(s, sizeof(s), "%ld,9,0,%s,%ld",
+    snprintf(s, sizeof(s), "%"PRIu32",9,0,%s,%"PRIu32,
                 instance->id,
                 instance->pdf1.hexID,
                 instance->pdf1.serialNumber

src/bl652.c
@@ -79,7 +79,7 @@ static void _BL652_processRx (BL652_Handle handle)
 
                 case 27:
                     sscanf(s, "%*d %s%n", handle->response.deviceName, &readPos);
-                    strncat(handle->response.deviceName, &s[readPos], sizeof(handle->response.deviceName));
+                    strncat(handle->response.deviceName, &s[readPos], sizeof(handle->response.deviceName)-1);
                     /* Remove trailing LF */
                     if (strlen(handle->response.deviceName) >= 1) {
                         handle->response.deviceName[strlen(handle->response.deviceName) - 1] = 0;

src/dfm/dfm.c
@@ -1,4 +1,5 @@
 
+#include <inttypes.h>
 #include <math.h>
 #if !defined(M_PI)
 #  define M_PI 3.14159265358979323846
@@ -161,7 +162,7 @@ LPCLIB_Result DFM_open (DFM_Handle *pHandle)
 /* Send position report */
 static void _DFM_sendKiss (DFM_InstanceData *instance)
 {
-    char s[120];
+    char s[128];
     char sAltitude[20];
     char sClimbRate[20];
     char sVelocity[8];
@@ -226,7 +227,7 @@ static void _DFM_sendKiss (DFM_InstanceData *instance)
             /* Nothing to do */
             break;
     }
-    snprintf(sSpecial, sizeof(sSpecial), "%lu", special);
+    snprintf(sSpecial, sizeof(sSpecial), "%"PRIu32, special);
 
     /* Climb rate and ground speed may not be available (Burkina Faso version) */
     sClimbRate[0] = 0;
@@ -244,7 +245,7 @@ static void _DFM_sendKiss (DFM_InstanceData *instance)
 
     /* Avoid sending the position if any of the values is undefined */
     if (isnan(latitude) || isnan(longitude)) {
-        length = sprintf((char *)s, "%ld,2,%.3f,,,,%s,%s,,,%s,,%s,,,,%.1f,,,,,",
+        length = sprintf((char *)s, "%"PRIu32",2,%.3f,,,,%s,%s,,,%s,,%s,,,,%.1f,,,,,",
                         instance->id,
                         f,                          /* Frequency [MHz] */
                         sAltitude,                  /* Altitude [m] */
@@ -255,7 +256,7 @@ static void _DFM_sendKiss (DFM_InstanceData *instance)
                         );
     }
     else {
-        length = sprintf((char *)s, "%ld,2,%.3f,,%.5lf,%.5lf,%s,%s,%s,%s,%s,,%s,,,,%.1f,,%d,,,%s",
+        length = sprintf((char *)s, "%"PRIu32",2,%.3f,,%.5lf,%.5lf,%s,%s,%s,%s,%s,,%s,,,,%.1f,,%d,,,%s",
                         instance->id,
                         f,                          /* Frequency [MHz] */
                         latitude,                   /* Latitude [degrees] */
@@ -276,7 +277,7 @@ static void _DFM_sendKiss (DFM_InstanceData *instance)
         SYS_send2Host(HOST_CHANNEL_KISS, s);
     }
 
-    length = sprintf(s, "%ld,2,0,%s,%.1f",
+    length = sprintf(s, "%"PRIu32",2,0,%s,%.1f",
                 instance->id,
                 instance->name,
                 instance->gps.ehpe
@@ -371,7 +372,7 @@ LPCLIB_Result DFM_processBlock (
                     | (handle->packet.gps[1].d1[11] <<  4)
                     | (handle->packet.gps[1].d1[12] <<  0)
                     ;
-            snprintf(log, sizeof(log), "%s,2,1,%07lX%05lX%08lX%05lX%08lX",
+            snprintf(log, sizeof(log), "%s,2,1,%07"PRIX32"%05"PRIX32"%08"PRIX32"%05"PRIX32"%08"PRIX32,
                         handle->instance->name,
                         confval,
                         gps0val1,
@@ -389,7 +390,7 @@ LPCLIB_Result DFM_processBlock (
                 LPCLIB_initEvent(&event, LPCLIB_EVENTID_APPLICATION);
                 event.opcode = APP_EVENT_HEARD_SONDE;
                 event.block = SONDE_DETECTOR_DFM;
-                event.parameter = (void *)((uint32_t)lrintf(rxFrequencyHz));
+                event.parameter = (void *)((uintptr_t)lrintf(rxFrequencyHz));
                 SYS_handleEvent(event);
 
                 result = LPCLIB_SUCCESS;

src/dfm/dfmconfig.c
@@ -123,7 +123,7 @@ dfm_config_unknown[channel].n++;
                 }
                 else {
                     /* Channels other than 0: If there is any gap (undetected subframe), reset detector */
-                    if (abs(delta - DFM_FRAME_LENGTH_MILLISEC) > DFM_FRAME_LENGTH_MILLISEC / 2.0f) {
+                    if (fabs(delta - DFM_FRAME_LENGTH_MILLISEC) > DFM_FRAME_LENGTH_MILLISEC / 2.0f) {
                         instance->confDetect.lastCh0Time = 0;
                     }
                 }

src/imet/imet.c
@@ -1,4 +1,5 @@
 
+#include <inttypes.h>
 #include <math.h>
 #if !defined(M_PI)
 #  define M_PI 3.14159265358979323846
@@ -113,7 +114,7 @@ static void _IMET_sendKiss (IMET_InstanceData *instance)
 
     /* Avoid sending the position if any of the values is undefined */
     if (isnan(latitude) || isnan(longitude)) {
-        length = sprintf((char *)s, "%ld,%s,%.3f,,,,%s,%s,,,,,,,,,%.1f,%.1f,0",
+        length = sprintf((char *)s, "%"PRIu32",%s,%.3f,,,,%s,%s,,,,,,,,,%.1f,%.1f,0",
                         instance->id,
                         sType,
                         f,                              /* Frequency [MHz] */
@@ -124,7 +125,7 @@ static void _IMET_sendKiss (IMET_InstanceData *instance)
                         );
     }
     else {
-        length = sprintf((char *)s, "%ld,%s,%.3f,,%.5lf,%.5lf,%s,%s,%s,%s,%.1f,%.1f,,,%.1f,,%.1f,%.1f,%d,%d,,%.1f",
+        length = sprintf((char *)s, "%"PRIu32",%s,%.3f,,%.5lf,%.5lf,%s,%s,%s,%s,%.1f,%.1f,,,%.1f,,%.1f,%.1f,%d,%d,,%.1f",
                         instance->id,
                         sType,
                         f,                              /* Frequency [MHz] */
@@ -149,7 +150,7 @@ static void _IMET_sendKiss (IMET_InstanceData *instance)
         SYS_send2Host(HOST_CHANNEL_KISS, s);
     }
 
-    length = sprintf(s, "%ld,6,0,%s",
+    length = sprintf(s, "%"PRIu32",6,0,%s",
                 instance->id,
                 instance->name
                 );
@@ -198,7 +199,7 @@ LPCLIB_Result IMET_processBlock (
             {
                 char log[80];
                 unsigned int i;
-                snprintf(log, sizeof(log), "%ld,6,1,",
+                snprintf(log, sizeof(log), "%"PRIu32",6,1,",
                             handle->instance->id);
                 for (i = 0; i < length; i++) {
                     snprintf(&log[strlen(log)], sizeof(log) - strlen(log), "%02X", p[i]);
@@ -238,7 +239,7 @@ LPCLIB_Result IMET_processBlock (
                     LPCLIB_initEvent(&event, LPCLIB_EVENTID_APPLICATION);
                     event.opcode = APP_EVENT_HEARD_SONDE;
                     event.block = SONDE_DETECTOR_IMET;
-                    event.parameter = (void *)((uint32_t)lrintf(rxSetFrequencyHz));
+                    event.parameter = (void *)((uintptr_t)lrintf(rxSetFrequencyHz));
                     SYS_handleEvent(event);
                 }
             }

src/jinyang/jinyang.c
@@ -115,7 +115,7 @@ static void _JINYANG_sendKiss (JINYANG_InstanceData *instance)
         velocity *= 3.6f;
     }
 
-    length = snprintf((char *)s, sizeof(s), "%ld,12,%.3f,,%.5lf,%.5lf,%.0f,,%.1f,%.1f,,,,,,,%.1f,,,%d,",
+    length = snprintf((char *)s, sizeof(s), "%"PRIu32",12,%.3f,,%.5lf,%.5lf,%.0f,,%.1f,%.1f,,,,,,,%.1f,,,%d,",
                     instance->id,
                     instance->rxFrequencyMHz,               /* Nominal sonde frequency [MHz] */
                     latitude,                               /* Latitude [degrees] */
@@ -148,7 +148,7 @@ static void _JINYANG_sendRaw (JINYANG_Handle handle, JINYANG_Packet *p1)
 
 
     snprintf(s, sizeof(s),
-                     "%ld,12,1,"
+                     "%"PRIu32",12,1,"
                      "%08"PRIX32"%08"PRIX32"%08"PRIX32"%08"PRIX32"%08"PRIX32
                      "%08"PRIX32"%08"PRIX32"%08"PRIX32"%08"PRIX32"%08"PRIX32,
                      handle->instance->id,

src/lpclib_config.h
@@ -1,3 +1,5 @@
+#ifndef __LPCLIB_CONFIG_H__
+#define __LPCLIB_CONFIG_H__
 
 #define LPCLIB_CLKPWR_POWER_PROFILES                    1
 
@@ -15,4 +17,6 @@
 #define SEMIHOSTING_RS92                                (SEMIHOSTING && 0)
 #define T32_TERM_BLOCKED
 #define T32_TERM_MEMORY_BLOCKED_SIZE                    128
+
+#endif /* __LPCLIB_CONFIG_H__ */
 

commit 4f41272eb15e71e72236cae99932af6650b71d2f

Fix typo that prevents RINEX update
Was introduced in 89da4a752882b74fc806eadd7a7d5aa485e661dc

src/ephemupdate.c
@@ -416,7 +416,7 @@ LPCLIB_Result EPHEMUPDATE_processCommand (EPHEMUPDATE_Handle handle, const char 
             {
                 uint32_t checksum = _EPHEMUPDATE_getEphemerisChecksum();
                 static char s[20];
-                sprintf(s, "%"PRIx32, checksum);
+                sprintf(s, "%"PRIu32, checksum);
                 response = s;
             }
             break;