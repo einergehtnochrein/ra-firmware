fda6d5f59a41f874cea47fbc7fc92465a4c3c62e
Rolf Meeser <rolf@black.local>
2021-12-25 22:16:19 +0100

    Update config sector structure

--- src/config.h
+++ src/config.h
@@ -12,7 +12,7 @@ __PACKED(struct _Config_t {
     uint16_t usbVID;
     uint16_t usbPID;
     uint16_t usbVERSION;
-    uint16_t __reserved036__;
+    uint16_t __reserved00A__;
 
     /* 0012  0x00C */
     float referenceFrequency;
@@ -25,7 +25,13 @@ __PACKED(struct _Config_t {
     char nameBluetooth[48];
     wchar_t usbVendorString[64];
     wchar_t usbProductString[64];
-    uint8_t __reserved__[588];
+    uint8_t __reserved1B0__[80];
+
+    /* 0512  0x200 */
+    uint16_t att_mtu;
+    uint16_t att_data_length;
+    uint16_t max_packet_length;
+    uint8_t __reserved200__[502];
 
     uint32_t crc;
 });
