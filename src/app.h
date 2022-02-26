2effedb11fcd905eea56213848e5296666d87785
df9dq <rolf@black.local>
2022-02-26 21:01:31 +0100

    Version 47.0

--- src/app.h
+++ src/app.h
@@ -6,8 +6,8 @@
 #include "scanner.h"
 #include "sys.h"
 
-#define FIRMWARE_VERSION_MAJOR              46
-#define FIRMWARE_VERSION_MINOR              1
+#define FIRMWARE_VERSION_MAJOR              47
+#define FIRMWARE_VERSION_MINOR              0
 #if !defined(FIRMWARE_NAME)
 #  define FIRMWARE_NAME                     ""
 #endif
