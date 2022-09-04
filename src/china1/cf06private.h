dc13784878f44a353a7e7da439f1dbd9b4020d9c
df9dq <rolf@black.local>
2022-09-04 15:11:56 +0200

    Add new sonde CF-06-AH
    Will add more sondes to a single detector "Asia(1)"

---
+++ src/china1/cf06private.h
@@ -0,0 +1,112 @@
+
+#ifndef __CF06PRIVATE_H
+#define __CF06PRIVATE_H
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "lpclib.h"
+#include "cf06.h"
+#include "gps.h"
+
+
+typedef __PACKED(union {
+    uint8_t dat8[100];
+    uint32_t dat32[25];
+}) CF06_RawData;
+
+
+typedef __PACKED(struct {
+    uint32_t reserved00;
+    uint32_t tow;
+    uint32_t longitude;
+    uint32_t latitude;
+    uint32_t altitude;
+    int16_t speed_east;
+    int16_t speed_north;
+    int16_t reserved18;
+    uint16_t reserved1A;
+    uint16_t reserved1C;
+    uint16_t reserved1E;
+    uint16_t reserved20;
+    uint8_t reserved0[6];
+    uint16_t crc;
+}) CF06_PayloadBlock1;
+
+
+typedef __PACKED(struct {
+    uint16_t reserved00;
+    uint8_t reserved0[27];
+    uint16_t crc;
+}) CF06_PayloadBlock2;
+
+
+typedef union {
+    CF06_RawData rawData;
+    __PACKED(struct {
+        uint8_t reserved00[3];
+        CF06_PayloadBlock1 inner;
+        uint8_t fec_inner[6];
+        uint8_t reserved33[10];
+        CF06_PayloadBlock2 outer;
+        uint8_t fec_outer[6];
+    });
+} CF06_Packet;
+
+
+typedef struct {
+    float temperature;                          /* Temperature [°C] */
+} CF06_CookedMetrology;
+
+
+typedef struct {
+    LLA_Coordinate observerLLA;
+} CF06_CookedGps;
+
+
+/* Data that needs to be stored for every instance. */
+typedef struct _CF06_InstanceData {
+    struct _CF06_InstanceData *next;
+    uint32_t id;
+    uint16_t frameCounter;
+    float rxFrequencyMHz;
+    uint32_t lastUpdated;
+
+    uint32_t lastGpsTime;
+    CF06_CookedGps gps;
+    CF06_CookedMetrology metro;
+} CF06_InstanceData;
+
+
+
+LPCLIB_Result _CF06_prepare (
+        CF06_Packet *packet,
+        CF06_InstanceData **instancePointer,
+        float rxFrequencyHz);
+
+/* Iterate through instances */
+bool _CF06_iterateInstance (CF06_InstanceData **instance);
+
+/* Remove an instance from the chain */
+void _CF06_deleteInstance (CF06_InstanceData *instance);
+
+/* Check CRC of outer/inner block */
+_Bool _CF06_checkCRCOuter (uint8_t *buffer, int length, uint16_t receivedCRC);
+_Bool _CF06_checkCRCInner (uint8_t *buffer, int length, uint16_t receivedCRC);
+/* Reed-Solomon error correction */
+LPCLIB_Result _CF06_checkReedSolomonOuter (uint8_t rawFrame[], int *pNumErrors);
+LPCLIB_Result _CF06_checkReedSolomonInner (uint8_t rawFrame[], int *pNumErrors);
+
+LPCLIB_Result _CF06_processPayloadBlock1 (
+        const CF06_PayloadBlock1 *payload,
+        CF06_CookedGps *cookedGps,
+        CF06_CookedMetrology *cookedMetro);
+LPCLIB_Result _CF06_processPayloadBlock2 (
+        const CF06_PayloadBlock2 *payload,
+        CF06_CookedGps *cookedGps,
+        CF06_CookedMetrology *cookedMetro);
+
+#endif
---
+++ src/china1/cf06utils.c
@@ -0,0 +1,132 @@
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lpclib.h"
+#include "cf06.h"
+#include "cf06private.h"
+#include "reedsolomon.h"
+
+
+
+/* Check CRC of a outer block */
+_Bool _CF06_checkCRCOuter (uint8_t *buffer, int length, uint16_t receivedCRC)
+{
+    CRC_Handle crc = LPCLIB_INVALID_HANDLE;
+    CRC_Mode crcMode;
+    bool result = false;
+
+    crcMode = CRC_makeMode(
+            CRC_POLY_CRCCCITT,
+            CRC_DATAORDER_NORMAL,
+            CRC_SUMORDER_NORMAL,
+            CRC_DATAPOLARITY_NORMAL,
+            CRC_SUMPOLARITY_NORMAL
+            );
+    if (CRC_open(crcMode, &crc) == LPCLIB_SUCCESS) {
+        CRC_seed(crc, 0x0000);
+        CRC_write(crc, buffer, length, NULL, NULL);
+
+        result = receivedCRC == CRC_read(crc);
+
+        CRC_close(&crc);
+    }
+
+    return result;
+}
+
+
+/* Check CRC of inner block */
+_Bool _CF06_checkCRCInner (uint8_t *buffer, int length, uint16_t receivedCRC)
+{
+    CRC_Handle crc = LPCLIB_INVALID_HANDLE;
+    CRC_Mode crcMode;
+    bool result = false;
+
+    crcMode = CRC_makeMode(
+            CRC_POLY_CRCCCITT,
+            CRC_DATAORDER_NORMAL,
+            CRC_SUMORDER_NORMAL,
+            CRC_DATAPOLARITY_NORMAL,
+            CRC_SUMPOLARITY_NORMAL
+            );
+    if (CRC_open(crcMode, &crc) == LPCLIB_SUCCESS) {
+        CRC_seed(crc, 0x0000);
+        CRC_write(crc, buffer, length, NULL, NULL);
+
+        result = receivedCRC == CRC_read(crc);
+
+        CRC_close(&crc);
+    }
+
+    return result;
+}
+
+
+/* Reed-Solomon error correction */
+LPCLIB_Result _CF06_checkReedSolomonOuter (uint8_t rawFrame[], int *pNumErrors)
+{
+    int numErrorsOuter = 0;
+    LPCLIB_Result result;
+    uint8_t _CF06_null;
+
+
+    uint8_t * _CF06_getDataAddressOuter (int index)
+    {
+        if (index < 89) {
+            return &rawFrame[91 - index];
+        }
+        else if (index >= 249) {
+            return &rawFrame[346 - index];
+        }
+        else {
+            _CF06_null = 0;
+            return &_CF06_null;
+        }
+    }
+
+    /* Reed-Solomon error correction for outer frame */
+    result = REEDSOLOMON_process(6, 1, _CF06_getDataAddressOuter, &numErrorsOuter);
+
+    if (pNumErrors) {
+        *pNumErrors = numErrorsOuter;
+    }
+
+    return result;
+}
+
+
+/* Reed-Solomon error correction */
+LPCLIB_Result _CF06_checkReedSolomonInner (uint8_t rawFrame[], int *pNumErrors)
+{
+    int numErrorsInner = 0;
+    LPCLIB_Result result;
+    uint8_t _CF06_null;
+
+
+    uint8_t * _CF06_getDataAddressInner (int index)
+    {
+        if (index < 42) {
+            return &rawFrame[44 - index];
+        }
+        else if (index >= 249) {
+            return &rawFrame[299 - index];
+        }
+        else {
+            _CF06_null = 0;
+            return &_CF06_null;
+        }
+    }
+
+
+    /* Reed-Solomon error correction for inner frame */
+    result = REEDSOLOMON_process(6, 1, _CF06_getDataAddressInner, &numErrorsInner);
+
+    if (pNumErrors) {
+        *pNumErrors = numErrorsInner;
+    }
+
+    return result;
+}
+
