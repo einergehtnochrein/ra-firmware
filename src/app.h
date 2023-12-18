254bf7ca630a08c27f4202b67d2651a34fe06e6a
df9dq <rolf@black.df9dq.de>
2023-12-18 23:27:36 +0100

    Version 54.0

--- src/app.h
+++ src/app.h
@@ -6,8 +6,8 @@
 #include "scanner.h"
 #include "sys.h"
 
-#define FIRMWARE_VERSION_MAJOR              53
-#define FIRMWARE_VERSION_MINOR              4
+#define FIRMWARE_VERSION_MAJOR              54
+#define FIRMWARE_VERSION_MINOR              0
 #if !defined(FIRMWARE_NAME)
 #  define FIRMWARE_NAME                     ""
 #endif
