404309a0c38751c73121d70c862454c482dae39d
df9dq <rolf@black.df9dq.de>
2024-02-05 00:49:08 +0100

    Update RS41 parameter structure

--- src/rs41/rs41private.h
+++ src/rs41/rs41private.h
@@ -311,8 +311,7 @@ typedef struct _RS41_InstanceData {
                 uint8_t mainboard[10];          /* 0x222: Name of mainboard (e.g. "RSM412") */
             }) names;
             __PACKED(struct {
-                uint8_t mainboard[8];           /* 0x22C: Serial number of mainboard (e.g. "L1123553") */
-                uint8_t reserved234;            /* 0x234: */
+                uint8_t mainboard[9];           /* 0x22C: Serial number of mainboard (e.g. "L1123553") */
                 uint8_t text235[10];            /* 0x235: "0000000000" */
                 uint16_t reserved23F;           /* 0x23F: */
                 uint16_t reserved241;           /* 0x241: */
