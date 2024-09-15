990c45e57f7d84c648d248423d5be4157b2ad21e
df9dq <rolf@dell.df9dq.de>
2024-09-16 01:36:16 +0200

    Version 56.2

--- src/app.h
+++ src/app.h
@@ -7,7 +7,7 @@
 #include "sys.h"
 
 #define FIRMWARE_VERSION_MAJOR              56
-#define FIRMWARE_VERSION_MINOR              1
+#define FIRMWARE_VERSION_MINOR              2
 #if !defined(FIRMWARE_NAME)
 #  define FIRMWARE_NAME                     ""
 #endif
